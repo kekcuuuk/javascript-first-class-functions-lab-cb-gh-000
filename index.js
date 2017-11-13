c61b3f9ca74590700df217ecaf47aa7d7ea93fa1
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-13 07:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+// ex1
 function countdown(callback) {
 	window.setTimeout(callback, 2000);
 }
 
+// ex2
+function createMultiplier(y) {
+	return x => Math.pow(x, y);
+}
+
+doubler = createMultiplier(2);
+tripler = createMultiplier(3);
+
+// ex3
 function createMultiplier(x, y) {
 	return Math.pow(x, y);
 }
+
 
 doubler = createMultiplier.bind(null, null, 2);
 tripler = createMultiplier.bind(null, null, 3);
